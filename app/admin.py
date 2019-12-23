cccd4daf339e8ecf3b615b854c4341f4863772d7
rockbrown <rockbrown.jp@gmail.com>
2019-12-23 15:48:10 +0000

    #3 Adminへ追加

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Posts
+admin.site.register(Posts)
+
+from .models import Mst_Post_Statuses
+admin.site.register(Mst_Post_Statuses)
