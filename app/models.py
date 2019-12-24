126dde55f8f9ea532ca746f06c58eeebf4c4feda
rockbrown <rockbrown.jp@gmail.com>
2019-12-24 19:24:15 +0000

    #6 テンプレート追加、__str__ 追記

--- app/models.py
+++ app/models.py
@@ -6,6 +6,9 @@ class Mst_Post_Statuses(models.Model):
     label = models.CharField(max_length=255)
     is_enabled = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.label
+
 
 class Posts(models.Model):
     id = models.AutoField(primary_key=True)
@@ -14,3 +17,6 @@ class Posts(models.Model):
     post_status_code = models.ForeignKey(Mst_Post_Statuses, on_delete=models.CASCADE)
     created = models.DateTimeField('date published')
     modofied = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
--- app/views.py
+++ app/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from app.models import Posts
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    posts = Posts
+    template = loader.get_template('posts/index.html')
+    context = {
+        'posts': posts,
+    }
+    return HttpResponse(template.render(context, request))
